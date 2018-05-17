db04285972679165b6c48f8eeb2bf80dd2e05e6c
James Llewellyn <james2.llewellyn@googlemail.com>
2018-05-17 15:32:50 -0400

    improved current typehead search without implementing vue type ahead

--- resources/assets/js/components/NavBarSearch.vue
+++ resources/assets/js/components/NavBarSearch.vue
@@ -1,18 +1,21 @@
 <template>
-    <div class="field has-addons navbar-item has-dropdown" :class="{'is-active' : showSearchOptions}" v-on-clickaway="hideDropdown">
-        <div class="control" :class="{'is-loading': isLoading}">
-            <input class="input search is-boarderless is-shadowless" v-model="query" type="text" placeholder="Search for a book title or an author" @keyup="asyncFind" @keyup.enter="search" @click.prevent="inFocus">
+    <div class="field has-addons navbar-item has-dropdown" :class="{'is-active' : showSearchOptions}"
+         v-on-clickaway="hideDropdown" @click="showDropdown()">
+        <div class="control" :class="{'is-loading': isSearching}">
+            <input class="input search is-boarderless is-shadowless" v-model="query" type="text"
+                   placeholder="Search for a book title or an author" @keyup="isTyping" @keyup.enter="search">
         </div>
         <div class="control">
             <a class="button is-primary" @click.prevent="search">
                 Search
             </a>
         </div>
-        <div class="navbar-dropdown search-dropdown">
+        <div class="navbar-dropdown search-dropdown" v-on-clickaway="hideDropdown">
             <div class="search-item" v-for="(book, key) in options" :key="key">
-                <div class="has-text-left"  @click="selected(book)">
+                <div class="has-text-left" @click="selected(book)">
                     <p class=" has-text-weight-bold">{{ book.title }}</p>
-                    <p v-for="(author, index) in book.authors" :key="index">{{ author }}<span v-if="index != 0 && index == book.authors.length - 1">, </span></p>
+                    <p v-for="(author, index) in book.authors" :key="index">{{ author }}<span
+                            v-if="index != 0 && index == book.authors.length - 1">, </span></p>
                 </div>
             </div>
         </div>
@@ -21,72 +24,73 @@
 
 <script>
     import store from '../store';
-    import Multiselect from 'vue-multiselect'
-    import { mixin as clickaway } from 'vue-clickaway'
+    import {mixin as clickaway} from 'vue-clickaway'
 
     export default {
         data() {
-            return{
+            return {
                 query: null,
-                selectedBook: null
+                selectedBook: null,
+                typingTimer: null,
+                doneTypingInterval: 1000,
+                isSearching: false,
+                options: null,
+                showSearchOptions: false
             }
         },
-        mixins: [ clickaway ],
-        components: { Multiselect },
-        computed:{
-            queryUrlEncoded(){
+        mixins: [clickaway],
+        computed: {
+            queryUrlEncoded() {
                 return _.replace(this.query, ' ', "+");
             },
-            options () { return this.$store.getters.getSearchAsyncResults },
-            isLoading(){return this.$store.getters.getIsLoadingOptions},
-            lastAsyncTimeStamp(){return this.$store.getters.getLastAsyncSearchTime},
-            showSearchOptions(){return this.$store.getters.showSearchOptions}
         },
-        methods:{
-            findIsbn(identifiers){
-                let index = _.findIndex(identifiers, ['type' , 'ISBN_10']);
-                if(!index < 0 ){
+        methods: {
+            isTyping() {
+                clearTimeout(this.typingTimer);
+                return this.typingTimer = setTimeout(this.pausedTyping, this.doneTypingInterval);
+            },
+            pausedTyping() {
+                this.isSearching = true
+                axios.get(`/api/goodreads?search=${this.query}`)
+                    .then((response) => {
+                        this.options = response.data.searchResults;
+                        this.isSearching = false
+                        this.showDropdown()
+                    }, (error) => {
+                        console.log(response);
+                    });
+            },
+            findIsbn(identifiers) {
+                let index = _.findIndex(identifiers, ['type', 'ISBN_10']);
+                if (!index < 0) {
                     return false;
                 }
                 return identifiers[index].identifier;
             },
-            urlEncoded(){
+            urlEncoded() {
                 return _.replace(this.query, ' ', "+");
             },
-            limitText (count) {
+            limitText(count) {
                 return `and ${count} other books`
             },
-            inFocus() {
-                if(!this.options){
-                    return false
-                }
-                return this.$store.commit('showSearchOptions')
+            showDropdown() {
+                this.showSearchOptions = true
             },
-            hideDropdown(){
-                return this.$store.commit('hideSearchOptions')
+            hideDropdown() {
+                return this.showSearchOptions = false
             },
-            asyncFind () {
-                if(this.query.length < 5){
-                    return false;
-                }
-                if(_.now() - this.lastAsyncTimeStamp < 2000){
-                    return false;
-                }
-                return this.$store.dispatch('searchAsyncFind', this.urlEncoded())
+            selected(book) {
+                this.hideDropdown()
+                Event.$emit('changePage', '/book/' + book.goodreads_id)
             },
-            selected(book){
-                this.$store.commit('hideSearchOptions')
-                Event.$emit('changePage', '/book/'+book.goodreads_id)
-            },
-            clearAll () {
+            clearAll() {
                 this.selectedBook = null
             },
-            search(query){
-                this.$store.commit('hideSearchOptions')
+            search(query) {
+                clearTimeout(this.typingTimer)
+                this.hideDropdown()
                 return this.$store.dispatch('searchGoodreads', this.urlEncoded(query))
             }
-        },
-        mounted() {
         }
     }
 </script>
